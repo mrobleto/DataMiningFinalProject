75923551c3d30d782842a753d3337c4c038bbeff
mrobleto <108373535+mrobleto@users.noreply.github.com>
2024-12-10 22:03:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -122,8 +122,9 @@ with col1:
 with col2:
     # Use HTML and Markdown for custom styling
     st.markdown(
-    "<h1 style='color:#4BAFA1;'>Ursula & Marisabel Your Travel Experts:</h1>
-    <h1 style='color:#4BAFA1;'>All You Need to Know! 💫</h1>",
+    """
+    <h1 style='color:#4BAFA1;'>Ursula & Marisabel Your Travel Experts:</h1>
+    <h1 style='color:#4BAFA1;'>All You Need to Know! 💫</h1>""",
     unsafe_allow_html=True
 )
     # st.title("Ursula & Marisabel Your Travel Experts: All You Need to Know!")
